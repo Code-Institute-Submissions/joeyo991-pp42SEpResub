7f927fc4c15f4d393cab07055237b936f73c6fd9
joeyo991 <joeyosull@gmail.com>
2022-06-23 11:18:56 +0000

    Modify and style the sign up form

--- users/views.py
+++ users/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 
 
 def sign_up(request):
     if request.method == 'POST': 
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('blog-index')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     context = {
         'form': form,
     }
