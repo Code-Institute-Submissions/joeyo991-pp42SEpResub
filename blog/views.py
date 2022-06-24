155d524f9db13f96e21ca7c8061283b66fe47509
joeyo991 <joeyosull@gmail.com>
2022-06-24 13:55:41 +0000

    Finish the post-edit page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import PostModel
-from .forms import PostModelForm
+from .forms import PostModelForm, PostUpdateForm
 
 # Create your views here.
 
@@ -33,4 +33,16 @@ def post_detail(request, pk):
 
 
 def post_edit(request, pk):
+    post = PostModel.objects.get(id=pk)
+    if request.method == 'POST':
+        form = PostUpdateForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog-post-detail', pk=post.id)
+    else:
+        form = PostUpdateForm(instance=post)
+    context = {
+        'post': post,
+        'form': form,
+    }
     return render(request, 'blog/post_edit.html', context)
