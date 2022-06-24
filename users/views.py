026bdf92aa2078e4aaef7d2b7d0f35b8604c5c99
joeyo991 <joeyosull@gmail.com>
2022-06-24 11:47:09 +0000

    Finish the edit profile modal on the profile page in views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
 
 # Create your views here.
 
@@ -20,4 +20,20 @@ def sign_up(request):
 
 
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        # User Update Form
+        u_form = UserUpdateForm(request.POST or None, instance=request.user)
+        # Profile Update Form
+        p_form = ProfileUpdateForm(request.POST or None, request.FILES or None, instance=request.user.profilemodel)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('users-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profilemodel)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'users/profile.html', context)
