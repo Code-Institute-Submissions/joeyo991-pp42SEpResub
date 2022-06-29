ba2312975168dd7f42b4ee47c79141853eba94b6
joeyo991 <joeyosull@gmail.com>
2022-06-29 13:39:06 +0000

    Fix error in base.html that was causing a 400 error when setting DEBUG to false

--- rightrecovery/settings.py
+++ rightrecovery/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rightrecovery.herokuapp.com', 'localhost']
 
@@ -152,4 +152,3 @@ LOGIN_REDIRECT_URL = 'blog-index'
 
 # Users must log in to access the site
 LOGIN_URL = 'users-login'
-
